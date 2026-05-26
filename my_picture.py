c5479b61955d3d4dc48ff54f50b76ca52e3234c9
CheeseIsWeird <nnguyen29@ssis.edu.vn>
2026-05-26 10:22:28 +0700

    Update my_picture.py

--- my_picture.py
+++ my_picture.py
@@ -1,48 +1,67 @@
 import simple_graphics as sg
 
 def draw_picture(width, height):
-    """Draws a static picture."""
-    
-    # Fill the background
-    sg.fill_background("white")
-    
-    # make some variables available
-    colors = ["red", "green", "blue", "cyan", "magenta", "yellow"]
-    
-    triangle_height = height/5
-    triangle_width = width / 3
-    
-    # Draw the tesselation
-    # code for red triangles
-    sg.set_fill_color(colors[0])
-    
-    # call fill
-    sg.fill_circle(450, 50,50)
+# SKY
+    sky_height = height * 3/8
 
-    # draw a mountain
-    sg.set_fill_color("#827e7e") # relatively dark gray
-    sg. fill_triangle(300, 150, 400, 20, 350, 150)
-    sg.set_fill_color("#c7c1c1") # lighter gray
-    sg. fill_triangle(350, 150, 400, 20, 550, 150)
+    sg.set_fill_color("skyblue")
+    sg.fill_rectangle(0, 0, width, sky_height)
 
-    # draw horizon
+#OCEAN
+    ocean_y = sky_height
+    ocean_height = height - ocean_y
+
+    sg.set_fill_color("deepskyblue")
+    sg.fill_rectangle(0, ocean_y, width, ocean_height)
+
+#SUN
+    sun_radius = height / 4
+
+    sun_center_x = width / 2
+    sun_center_y = sky_height
+
+    sg.set_fill_color("yellow")
+
+#Draw top half of circle
+    sg.fill_arc(
+        sun_center_x - sun_radius,
+        sun_center_y - sun_radius,
+        sun_radius * 2,
+        sun_radius * 2,
+        0,
+        180
+    )
+
+#CLOUDS
+    
+    sg.set_fill_color("white")
+
+    #Cloud 1
+    sg.fill_circle(120, 80, 25)
+    sg.fill_circle(150, 70, 30)
+    sg.fill_circle(180, 80, 25)
+
+    #Cloud 2
+    sg.fill_circle(420, 100, 20)
+    sg.fill_circle(445, 90, 28)
+    sg.fill_circle(475, 100, 20)
+
+#BIRDS
     sg.set_outline_color("black")
-    sg.set_line_thickness(1)
-    sg.draw_line(0, 150, 600, 150
-    
-    # Define the points the curve should bend through
-    river_points = [
-        (100, 150), # Start point
-        (300, 200), # Bends towards here
-        (200, 350), # Bends back here
-        (500, 500)  # End point
-    ]
+    sg.set_line_thickness(2)
 
-    sg.set_outline_color("blue")
-    sg.set_line_thickness(8)
-    sg.draw_curve(river_points)
-    
+    #Bird 1
+    sg.draw_line(250, 70, 260, 60)
+    sg.draw_line(260, 60, 270, 70)
+
+    #Bird 2
+    sg.draw_line(320, 90, 330, 80)
+    sg.draw_line(330, 80, 340, 90)
+
+    #Bird 3
+    sg.draw_line(500, 60, 510, 50)
+    sg.draw_line(510, 50, 520, 60)
+
 
 if __name__ == "__main__":
-    # Launch the wrapper; only edit starting dimensions of canvas if you would like to
     sg.start(draw_picture, 600, 400)
